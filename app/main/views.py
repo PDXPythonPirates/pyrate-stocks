1dd9fb2d4f70fe3ef7000820909630600c522ef1
matti <lime811@gmail.com>
2021-01-25 14:38:59 -0800

    added mock user info and updated views

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__, template_folder='../templates')
 
@@ -6,11 +6,25 @@ app = Flask(__name__, template_folder='../templates')
 def home():
     return '<h1>Home Page</h1>'
 
-@app.route('/login')
-def login():
-    context = {
-
+@app.route('/login/<username>/<password>')
+def login(username, password):
+    
+    users = {
+        'matthias': {
+            'password': 'thispass',
+            'creation-date': '01/02/21',
+            'location': 'Portland, OR'
+        },
+        'xuehong': {
+            'password': 'anotherpass',
+            'creation-date': '01/02/21',
+            'location': 'Portland, OR'
+        }
     }
+
+    if username in users and users[username][password] == password:
+        return redirect('/')
+
     return render_template('login.html')
 
 if __name__=='__main__':

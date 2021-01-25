1d38dd54077480bc4f6088e40cbde51c0ef38d98
matti <lime811@gmail.com>
2021-01-25 14:10:24 -0800

    updated views

--- app/main/views.py
+++ app/main/views.py
@@ -2,20 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return '<h1>Home Page </h1>'
+
 @app.route('/login')
 def login():
     context = {
 
     }
     return render_template('login.html')
-
-@app.route('/home')
-def home():
-    return '<h1>Home Page </h1>'
-
-@app.route('/about')
-def about():
-    return '<h1>About Page </h1>'
-
+    
 if __name__=='__main__':
     app.run()

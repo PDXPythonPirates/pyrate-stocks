869fde865cc979eed15633ac32f8a3bb964f543d
matti <lime811@gmail.com>
2021-01-31 14:20:26 -0800

    added csv read/write for login/signup (working)

--- app/main/forms.py
+++ app/main/forms.py
@@ -12,17 +12,7 @@ class LogoutForm(FlaskForm):
     logout = SubmitField('Submit')
 
 class SignUpForm(FlaskForm):
-    # Data fields that connect to inputs fields on signup.html in templates.
-    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=15)])
-    firstname = StringField('First Name', validators=[InputRequired()])
-    lastname = StringField('Last Name', validators=[InputRequired()])
-    email = StringField('Email', validators=[InputRequired(), Email(message='Invalid Email'), Length(max=20)])
-    phone = StringField('Phone', validators=[InputRequired()])
-    address = StringField('Address', validators=[InputRequired()])
-    addressLine2 = StringField('Address Line 2', validators=None)
-    city = StringField('City', validators=[InputRequired()])
-    state = StringField('State', validators=[InputRequired()])
-    zipcode = StringField('Zip Code', validators=[InputRequired()])
-    # Submit Button
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    email = StringField('Email', validators=[InputRequired()])
     submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -2,19 +2,49 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_bootstrap import Bootstrap
 from forms import LoginForm, LogoutForm, SignUpForm
 from keychain import Keys
-import json
+import csv
 
 
 app = Flask(__name__, template_folder='../templates')
 app.config['SECRET_KEY'] = Keys.secret()
 Bootstrap(app)
 
+##### HOME #####
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
 
     return render_template('home.html')
 
+
+##### ADD USER #####
+
+def append_list_as_row(file_name, user_data):
+    with open(file_name, 'w', newline='') as file:
+        print('test2', user_data)
+        writer = csv.writer(file, delimiter=',')
+        writer.writerow(user_data)
+
+
+##### SIGNUP #####
+
+@app.route('/signup/', methods=['GET', 'POST'])
+def signup():
+    form = SignUpForm()
+
+    print('signup page accessed')
+    if form.validate_on_submit():
+        print('test0')
+        user_data = [form.username.data, form.password.data, form.email.data]
+        print('test1', user_data)
+        append_list_as_row('app/main/user_data.csv', user_data)
+
+        return redirect(url_for('login'))
+
+    return render_template('signup.html', form=form)
+
+
+##### LOGIN #####
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
@@ -25,19 +55,31 @@ def login():
         password = form.password.data
 
         # Check user_data.json for username & password match
-        file = open('user_data.json',)
-        data = json.load(file)
+        with open('app/main/user_data.csv', 'r') as file:
+            user = file.readline()
+            user = list(user.split(','))
+            user = [x.strip('\n') for x in user]
+
         
         # Not sure if the second check is a real thing. I think this will throw an error.
-        if username in data['users']['username'].values() and password == data['users'][username]['password']:
-            print(f'successful login for {username}.')
-            file.close()
-            return render_template('profile.html', form=form, username=username, display_message=f'Welcome, {username}')
+        if username == user[0] and password == user[1]:
+            return render_template('profile.html', form=LogoutForm(), username=username, display_message=f'Welcome, {username}')
         else:
             return render_template('login.html', form=form, display_message='Incorrect Login')
 
     return render_template('login.html', form=form, display_message='User Login')
 
+
+##### PROFILE #####
+
+@app.route('/profile/', methods=['GET', 'POST'])
+def profile():
+    form = LogoutForm()
+
+    return render_template('profile.html', form=form)
+
+
+##### LOGOUT #####
 
 @app.route('/logout/', methods=['GET', 'POST'])
 def logout():
@@ -51,28 +93,7 @@ def logout():
         return render_template('profile.html')
 
 
-@app.route('/profile/', methods=['GET', 'POST'])
-def profile():
-    form = LogoutForm()
-
-    return render_template('profile.html', form=form)
-
-
-@app.route('/signup/', methods=['GET', 'POST'])
-def signup():
-    form = SignUpForm()
-
-    if form.validate_on_submit():
-        form = request.form
-
-        # insert new user data into user_data.json here
-
-        #
-
-        return redirect(url_for('login'))
-
-    return render_template('signup.html', form=form)
-
+##### RUN APP #####
 
 if __name__=='__main__':
     app.run(debug=True)

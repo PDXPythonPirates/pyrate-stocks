8c0a47dfceb493eebf8fa647c0e4b38eb430fba3
matti <lime811@gmail.com>
2021-02-03 17:44:28 -0800

    added ticker class and connected database to app.

--- app/main/views.py
+++ app/main/views.py
@@ -1,14 +1,35 @@
 from flask import Flask, render_template, redirect, request, url_for
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
 from forms import LoginForm, LogoutForm, SignUpForm
 from keychain import Keys
-import csv
 import json
-
 
 app = Flask(__name__, template_folder='../templates')
 app.config['SECRET_KEY'] = Keys.secret()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 Bootstrap(app)
+db = SQLAlchemy(app)
+
+class ticker(db.Model):
+    id = db.Column('id', db.Integer, primary_key=True)
+    symbol = db.Column(db.String(48))
+    current_price = db.Column(db.String(48))
+    market_high = db.Column(db.String(48))
+    market_low = db.Column(db.String(48))
+    market_open = db.Column(db.String(48))
+    market_close = db.Column(db.String(48))
+
+    def __init__(self, symbol, current_price, market_high, market_low, market_open, market_close):
+        self.symbol = symbol
+        self.current_price = current_price
+        self.market_high = market_high
+        self.market_low = market_low
+        self.market_open = market_open
+        self.market_close = market_close
 
 
 ##### HOME #####

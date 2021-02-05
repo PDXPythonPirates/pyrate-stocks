4e5003e8de9a26816dd212ae4c3afe6699fb4c09
cassjsdev <cassjes.m@gmail.com>
2021-02-05 13:03:17 -0800

    modified dashboard, added data, need to pass symbol, find current price, & isolate prices

--- app/main/views.py
+++ app/main/views.py
@@ -1,30 +1,78 @@
 from flask import Flask, render_template, redirect, request, url_for
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-
 from forms import LoginForm, LogoutForm, SignUpForm
 from keychain import Keys
+
 import json
+import datetime as dt
+import yfinance as yf
+import pandas as pd
 
 app = Flask(__name__, template_folder='../templates')
 app.config['SECRET_KEY'] = Keys.secret()
+Bootstrap(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-Bootstrap(app)
 db = SQLAlchemy(app)
 db.create_all()
 
-##### DB CLASS #####
-
 class Ticker(db.Model):
     id = db.Column('id', db.Integer, primary_key=True)
-    symbol = db.Column(db.String(50))
-    current_price = db.Column(db.String(50))
-    market_high = db.Column(db.String(50))
-    market_low = db.Column(db.String(50))
-    market_open = db.Column(db.String(50))
-    market_close = db.Column(db.String(50))
+    symbol = db.Column(db.String(10))
+    
+    ####### SAVE JUST IN CASE NEED TO CHANGE BACK ########
+    # class Ticker(db.Model):
+    # id = db.Column('id', db.Integer, primary_key=True)
+    # symbol = db.Column(db.String(10))
+    # current_price = db.Column(db.Float)
+    # market_high = db.Column(db.Float)
+    # market_low = db.Column(db.Float)
+    # market_open = db.Column(db.Float)
+    # market_close = db.Column(db.Float)
+
+### TEST TO PRINT STOCK DATA ###
+ticker = yf.Ticker('TSLA')
+df = ticker.info
+print(df)
+
+
+##### DASHBOARD #####
+
+@app.route("/dashboard")
+def dashboard():
+    ticker_list = Ticker.query.all()
+    ticker = yf.Ticker('TSLA') # temporary.... how to pass 'symbol' from Ticker class?
+    df = ticker.history(period="today")
+    current_price = df['Close'] # Where do I get current price??? 
+    market_high = df['High']
+    market_low = df['Low']
+    market_open = df['Open']
+    market_close = df['Close']
+    return render_template("dashboard.html", 
+                           ticker_list=ticker_list, 
+                           current_price=current_price, 
+                           market_high=market_high, 
+                           market_low=market_low,
+                           market_open=market_open,
+                           market_close=market_close)
+    
+# Add a new symbol to track in DB
+@app.route("/add", methods=["POST"])
+def add():
+    symbol = request.form.get("symbol")
+    new_ticker = Ticker(symbol=symbol)
+    print(new_ticker)
+    db.session.add(new_ticker)
+    db.session.commit()
+    return redirect('/dashboard')
+
+# Delete a symbol being tracked in DB             
+@app.route("/delete/<int:ticker_id>")   
+def delete(ticker_id):
+    Ticker.query.filter_by(id=ticker_id).delete()
+    db.session.commit()
+    return redirect('/dashboard')
 
 
 ##### SIGNUP #####
@@ -111,33 +159,6 @@ def logout():
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('home.html')
-    
-    
-
-    
-##### DASHBOARD #####
-
-@app.route("/dashboard")
-def dashboard():
-    ticker_list = Ticker.query.all()
-    return render_template("dashboard.html", ticker_list=ticker_list)
-
-# Add a new symbol to track in DB
-@app.route("/add", methods=["POST"])
-def add():
-    symbol = request.form.get("symbol")
-    new_ticker = Ticker(symbol=symbol)
-    print(new_ticker)
-    db.session.add(new_ticker)
-    db.session.commit()
-    return redirect('/dashboard')
-
-# Delete a symbol being tracked in DB             
-@app.route("/delete/<int:ticker_id>")   
-def delete(ticker_id):
-    Ticker.query.filter_by(id=ticker_id).delete()
-    db.session.commit()
-    return redirect('/dashboard')
 
 
 ##### RUN APP #####

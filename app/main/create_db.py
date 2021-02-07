5750e37abdcd6116cec45277e8a23d994d9c5521
midCenturyDude7 <mgriffes@gmail.com>
2021-02-06 18:58:41 -0800

    Minor edits to new user db entry instructions

--- app/main/create_db.py
+++ app/main/create_db.py
@@ -9,13 +9,13 @@ The following commands need to be issued on python command line one by one.
 
 from views import db
 from views import Account
-xuehong = account(username='xuehong', email='xuehong@example.com', password = 'qqq', stocks = 'aapl')
+xuehong = Account(username='xuehong', email='xuehong@example.com', password='qqq', stocks='aapl')
 
 db.session.add(xuehong)
 db.session.commit()
 
 # check account entries
-u=account.query.filter_by(username='xuehong').first()
+u=Account.query.filter_by(username='xuehong').first()
 u.username
 
 """

095520425aa2520f09e467de2503f12a591327a7
midCenturyDude7 <mgriffes@gmail.com>
2021-02-09 20:40:09 -0800

    Minor edit to class Config()

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard-to-guess-string'
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').Lower() in ['true', 'on', '1']
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FIN_APP_MAIL_SUBJECT_PREFIX = '[Financial_Application]'

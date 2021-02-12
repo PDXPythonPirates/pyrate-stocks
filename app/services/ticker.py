f3774469cb0baca1672de7d3f7078f634764ac52
matti <lime811@gmail.com>
2021-02-12 14:11:06 -0800

    adjusted imports

--- app/services/ticker.py
+++ app/services/ticker.py
@@ -1,6 +1,5 @@
 from flask import render_template, redirect, session
 import yfinance as yf
-
 from app.main import ticker
 from app.models import Ticker, Account
 from app.main.forms import LoginForm, LogoutForm, UpdateForm

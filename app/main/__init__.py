e26c2a923268525dda7b64f728a4c42c223ba962
matti <lime811@gmail.com>
2021-02-12 16:38:24 -0800

    removed uneeded imports

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -5,4 +5,3 @@ ticker_bp = Blueprint('ticker_bp', __name__, url_prefix='/ticker')
 user_bp = Blueprint('user_bp', __name__, url_prefix='/user')
 
 from app.main import views
-from app.services import ticker, user

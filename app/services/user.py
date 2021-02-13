82b930aa50b75c7a5f1e7114737e7647c50e5486
Xuehong <xuehong.liu.pdx@gmail.com>
2021-02-13 14:37:29 -0800

    user service working except update function

--- app/services/user.py
+++ app/services/user.py
@@ -9,6 +9,7 @@ from app.main.forms import LoginForm, UpdateForm, SignUpForm
 @user_bp.route('/signup/', methods=['GET', 'POST'])
 def signup():
     if current_user.is_authenticated:
+        flash('You already signed in!')
         return redirect(url_for('main_bp.dashboard'))
     sform = SignUpForm()
     lform = LoginForm()
@@ -20,20 +21,24 @@ def signup():
         db.session.commit()
         flash('You were successfully logged in')
         return render_template('login.html', form=lform)
+    flash('Please sign Up')
     return render_template('signup.html', title='Signup', form=sform)
     
 
 @user_bp.route('/login/', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
+        flash('You already signed in!')
         return redirect(url_for('main_bp.dashboard'))
 
     lform = LoginForm()
     if lform.validate_on_submit():
         user = Account.query.filter_by(username=lform.username.data).first()
         if user is None or not user.check_password(lform.password.data):
+            flash('Please sign up first!')
             return redirect(url_for('user_bp.login'))
 
+        flash('You are logged in.')
         login_user(user, remember=lform.remember.data)
         return redirect(url_for('main_bp.dashboard'))
 
@@ -43,18 +48,22 @@ def login():
 @login_required
 def update():
     uform = UpdateForm()
-        
     if uform.validate_on_submit():
-        user = Account(username=uform.username.data, email=uform.email.data, password_hash='xxx', stocks=uform.stocks.data)
-        user.set_password(uform.password.data)
-        db.session.add(user)
+        current_user.username = uform.username.data
+        current_user.email = uform.email.data
+        current_user.password = uform.password.data
+        current_user.set_password(uform.password.data)
         db.session.commit()
-        return render_template('dashboard.html', form=uform, display_message='Account Updated')
-    return render_template('update.html', title='Update', form=uform)
+        flash('Your changes have been saved.')
+        return redirect(url_for('main_bp.dashboard'))
+      
+    return render_template('update.html', title='Update',
+                           form=uform)
 
 
 @user_bp.route('/logout/', methods=['GET', 'POST'])
 @login_required
 def logout():
     logout_user()
+    flash('You are logged out!')
     return redirect(url_for('main_bp.home'))

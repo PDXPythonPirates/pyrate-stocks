c209a99003e2b389ca236ccdf9490ca05094d7df
midCenturyDude7 <mgriffes@gmail.com>
2021-02-19 15:53:13 -0800

    initial configuration for Flask-Migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
 from config import config
 
 db = SQLAlchemy()
 login = LoginManager()
+migrate = Migrate(db)
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -14,6 +16,7 @@ def create_app(config_name):
     from app.models import db
     db.init_app(app)
     login.init_app(app)
+    migrate.init_app(app, db)
     
 
     from app.main import main_bp

be279f1398d28936b22f62b4c95b688e36489b3c
midCenturyDude7 <mgriffes@gmail.com>
2021-02-20 22:28:21 -0800

    fixed bug by removing arg from create_app()

--- fin_app.py
+++ fin_app.py
@@ -3,7 +3,7 @@ from app import create_app, db
 from app.models import Account
 from flask_migrate import Migrate
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app = create_app()
 migrate = Migrate(app, db)
 
 @app.shell_context_processor

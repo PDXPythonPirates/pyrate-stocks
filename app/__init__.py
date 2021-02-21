f77cb5cc30dccc2d1d8dd213cd0fa4bd391a2e50
midCenturyDude7 <mgriffes@gmail.com>
2021-02-21 10:29:12 -0800

    edited/simplified  string variable db maps to'

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ db = SQLAlchemy()
 login = LoginManager()
 migrate = Migrate()
 
-def create_app(config_class=Config):
+def create_app():
     app = Flask(__name__)
 
     # commenting out previous configuration
@@ -18,7 +18,7 @@ def create_app(config_class=Config):
 
     # work around to address error msg: 'Neither SQLALCHEMY_DATABASE_URI nor SQLALCHEMY_BINDS is set'
     app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY') or 'hard-to-guess-string'
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL') or 'sqlite:///' + 'C:\\...path...\\app.db'
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL') or 'sqlite:///' + 'app.db'
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
             
     from app.models import db

c91aaa69d1d1ff4849b5254e499226e9fce06017
Xuehong <xuehong.liu.pdx@gmail.com>
2021-02-24 08:45:31 -0800

    working error handler

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, flash, url_for, request
+from flask import render_template, redirect, url_for, request
 from flask_login import current_user
 from sqlalchemy.orm import synonym
 from app.main import main_bp

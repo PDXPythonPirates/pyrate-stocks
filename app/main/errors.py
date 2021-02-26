960123f329a3e530b7fcaeb42989f34c40970148
Xuehong <xuehong.liu.pdx@gmail.com>
2021-02-25 16:14:25 -0800

    working error handler

--- app/main/errors.py
+++ app/main/errors.py
@@ -2,11 +2,15 @@ from flask import render_template
 from app import db
 from app.main import main_bp
 
-@main_bp.errorhandler(404)
+@main_bp.app_errorhandler(403)
 def not_found_error(error):
+    return render_template('403.html'), 403
+
+@main_bp.app_errorhandler(404)
+def forbidden_error(error):
     return render_template('404.html'), 404
 
-@main_bp.errorhandler(500)
+@main_bp.app_errorhandler(500)
 def internal_error(error):
     db.session.rollback()
     return render_template('500.html'), 500

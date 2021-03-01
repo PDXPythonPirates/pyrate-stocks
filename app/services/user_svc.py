194b149d709ed177ba6bd1e10da70f3ce75ff1ef
Xuehong <xuehong.liu.pdx@gmail.com>
2021-03-01 14:22:20 -0800

    fixed update passwd issue

--- app/services/user_svc.py
+++ app/services/user_svc.py
@@ -85,10 +85,12 @@ class UserService():
                     return render_template('update.html', form=uform)
                 if uform.validate_on_submit():
                     uform.populate_obj(user)
-                    user.set_password(uform.password.data)
-                    db.session.commit()
-                    flash('Your inforamtion is updated!')
-                    return redirect(url_for('main_bp.dashboard'))
+                    if uform.password.data:
+                        user.set_password(uform.password.data)
+                    else:
+                        db.session.commit()
+                        flash('Your inforamtion is updated!')
+                        return redirect(url_for('main_bp.dashboard'))
             
             uform.username.data = current_user.username
             uform.email.data = current_user.email
